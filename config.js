5464ccdfc7502485dde2c5385e9a6ff19b4817b0
Mark Angel-Trueman <mark@Marks-iMac.local>
2018-04-28 14:19:03 +0100

    oops

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let config = {
     port: 3000,
     auth: {
-        client_id: 'steemboard.app',
+        client_id: process.env.PRODUCTION ? 'becquerel' : 'steemboard.app',
         redirect_uri: process.env.SC_REDIR_URL ? process.env.SC_REDIR_URL : 'http://localhost:3001/auth'
     },
     session: {
